719e377cf44061a02aac2fc54a871e6187002850
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-03-21 01:07:18 -0400

    [IMP] Code refactor.

--- src/App.js
+++ src/App.js
@@ -4,21 +4,16 @@ import Header from './components/Header';
 import LandingSection from './components/LandingSection';
 import ProjectsSection from './components/ProjectsSection';
 import Alert from "./components/Alert";
-import { AlertProvider } from './context/alertContext';
 
 function App() {
   return (
-    <AlertProvider>
       <main>
-
         <Header />
         <LandingSection />
         <ProjectsSection />
         <ContactMeSection />
         <Alert />
-
       </main>
-    </AlertProvider>
   );
 }
 
